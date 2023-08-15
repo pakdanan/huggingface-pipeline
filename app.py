6d4c544b9174b8f157de7991d15d36e39898f669
PakDanan <danan_p@yahoo.com>
2023-08-15 14:23:31 +0700

    change summarizer model

--- app.py
+++ app.py
@@ -39,10 +39,10 @@ elif option == "Named Entity Recognition (cahya/bert-base-indonesian-NER)":
         answer = ner(text)
         st.write(answer)
 elif option == "Summarization (default model)":
-    summarizer = pipeline("summarization")
+    #summarizer = pipeline("summarization")
     article = st.text_area(label="Paste Article")
     if st.button("Summarize"):
-        summary = summarizer(article, max_length=400, min_length=30)
+        summary = summarize_T5(article) #summarizer(article, max_length=400, min_length=30)
         st.write(summary)
 elif option == "Translation (default model)":
     translator = pipeline("translation_en_to_de")
@@ -50,3 +50,21 @@ elif option == "Translation (default model)":
     if st.button("Translate"):
         translation = translator(text)
         st.write(translation)
+
+
+def summarize_T5(text):
+    from transformers import T5Tokenizer, T5ForConditionalGeneration
+    tokenizer = T5Tokenizer.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
+    model = T5ForConditionalGeneration.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
+    # generate summary
+    input_ids = tokenizer.encode(text, return_tensors='pt')
+    summary_ids = model.generate(input_ids,
+            max_length=100, 
+            num_beams=2,
+            repetition_penalty=2.5, 
+            length_penalty=1.0, 
+            early_stopping=True,
+            no_repeat_ngram_size=2,
+            use_cache=True)
+    summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary_text
