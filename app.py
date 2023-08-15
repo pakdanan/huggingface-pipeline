6deb3c421c4c1cbd50ba0eee370a6e41ae40609e
PakDanan <danan_p@yahoo.com>
2023-08-15 13:45:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,38 +13,38 @@ option = st.selectbox(
     ],
 )
 
-if option == "Classify Text":
+if option == "Classify Text (default model)":
     text = st.text_area(label="Enter text")
     if st.button("Classify"):
         classifier = pipeline("sentiment-analysis")
         answer = classifier(text)
         st.write(answer)
-elif option == "Question Answering":
+elif option == "Question Answering (default model)":
     q_a = pipeline("question-answering")
     context = st.text_area(label="Enter context")
     question = st.text_area(label="Enter question")
     if st.button("Answer"):
         answer = q_a({"question": question, "context": context})
         st.write(answer)
-elif option == "Text Generation":
+elif option == "Text Generation (default model)":
     text = st.text_area(label="Enter text")
     if st.button("Generate"):
         text_generator = pipeline("text-generation")
         answer = text_generator(text)
         st.write(answer)
-elif option == "Named Entity Recognition":
+elif option == "Named Entity Recognition (cahya/xlm-roberta-large-indonesian-NER)":
     text = st.text_area(label="Enter text")
     if st.button("Recognize"):
         ner = pipeline("token-classification", model="cahya/xlm-roberta-large-indonesian-NER")
         answer = ner(text)
         st.write(answer)
-elif option == "Summarization":
+elif option == "Summarization (default model)":
     summarizer = pipeline("summarization")
     article = st.text_area(label="Paste Article")
     if st.button("Summarize"):
         summary = summarizer(article, max_length=400, min_length=30)
         st.write(summary)
-elif option == "Translation":
+elif option == "Translation (default model)":
     translator = pipeline("translation_en_to_de")
     text = st.text_area(label="Enter text")
     if st.button("Translate"):
