220d2ca353eb58f09597a131548e215ea7abf687
PakDanan <danan_p@yahoo.com>
2023-08-15 14:37:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import streamlit as st
 from transformers import pipeline
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+def summarize_T5(text):
+    tokenizer = T5Tokenizer.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
+    model = T5ForConditionalGeneration.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
+    # generate summary
+    input_ids = tokenizer.encode(text, return_tensors='pt')
+    summary_ids = model.generate(input_ids,
+            max_length=100, 
+            num_beams=2,
+            repetition_penalty=2.5, 
+            length_penalty=1.0, 
+            early_stopping=True,
+            no_repeat_ngram_size=2,
+            use_cache=True)
+    summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary_text
 
 option = st.selectbox(
     "Select an Option",
@@ -52,19 +69,3 @@ elif option == "Translation (default model)":
         st.write(translation)
 
 
-def summarize_T5(text):
-    from transformers import T5Tokenizer, T5ForConditionalGeneration
-    tokenizer = T5Tokenizer.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
-    model = T5ForConditionalGeneration.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
-    # generate summary
-    input_ids = tokenizer.encode(text, return_tensors='pt')
-    summary_ids = model.generate(input_ids,
-            max_length=100, 
-            num_beams=2,
-            repetition_penalty=2.5, 
-            length_penalty=1.0, 
-            early_stopping=True,
-            no_repeat_ngram_size=2,
-            use_cache=True)
-    summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return summary_text
