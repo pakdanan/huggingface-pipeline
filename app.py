5305b52b992231e22de53d81d6613dbdbf011f8e
PakDanan <danan_p@yahoo.com>
2023-08-15 15:11:39 +0700

    change NER, Sentiment Analysis, and Summarization model to Indonesian

--- app.py
+++ app.py
@@ -2,13 +2,39 @@ import streamlit as st
 from transformers import pipeline
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
+@st.cache_resource  # 👈 Add the caching decorator
+def load_sentiment_analysis_model():
+    return pipeline(
+    "sentiment-analysis",
+    model="w11wo/indonesian-roberta-base-sentiment-classifier",
+    tokenizer="w11wo/indonesian-roberta-base-sentiment-classifier",
+    return_all_scores=True
+    )    
+    
+@st.cache_resource  # 👈 Add the caching decorator
+def load_ner_model():
+    return pipeline(
+    "ner",
+    model="cahya/bert-base-indonesian-NER",
+    tokenizer="cahya/bert-base-indonesian-NER",
+    grouped_entities=True
+    )
+
+@st.cache_resource  # 👈 Add the caching decorator
+def load_T5Tokenizer():
+    return T5Tokenizer.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
+
+@st.cache_resource  # 👈 Add the caching decorator
+def load_T5Model():
+    return T5ForConditionalGeneration.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
+
 def summarize_T5(text):
-    tokenizer = T5Tokenizer.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
-    model = T5ForConditionalGeneration.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
+    tokenizer = load_T5Tokenizer()    #T5Tokenizer.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
+    model =  load_T5Model()    #T5ForConditionalGeneration.from_pretrained("panggi/t5-base-indonesian-summarization-cased")
     # generate summary
     input_ids = tokenizer.encode(text, return_tensors='pt')
     summary_ids = model.generate(input_ids,
-            max_length=100, 
+            max_length=220, 
             num_beams=2,
             repetition_penalty=2.5, 
             length_penalty=1.0, 
@@ -21,16 +47,16 @@ def summarize_T5(text):
 option = st.selectbox(
     "Select an Option",
     [
-        "Classify Text (default model)",
+        "Classify Text (w11wo/indonesian-roberta-base-sentiment-classifier)",
         "Question Answering (default model)",
         "Text Generation (default model)",
         "Named Entity Recognition (cahya/bert-base-indonesian-NER)",
-        "Summarization (default model)",
+        "Summarization (panggi/t5-base-indonesian-summarization-cased)",
         "Translation (default model)",
     ],
 )
 
-if option == "Classify Text (default model)":
+if option == "Classify Text (w11wo/indonesian-roberta-base-sentiment-classifier)":
     text = st.text_area(label="Enter text")
     if st.button("Classify"):
         classifier = pipeline("sentiment-analysis")
@@ -55,7 +81,7 @@ elif option == "Named Entity Recognition (cahya/bert-base-indonesian-NER)":
         ner = pipeline("token-classification", model="cahya/bert-base-indonesian-NER")
         answer = ner(text)
         st.write(answer)
-elif option == "Summarization (default model)":
+elif option == "Summarization (panggi/t5-base-indonesian-summarization-cased)":
     #summarizer = pipeline("summarization")
     article = st.text_area(label="Paste Article")
     if st.button("Summarize"):
